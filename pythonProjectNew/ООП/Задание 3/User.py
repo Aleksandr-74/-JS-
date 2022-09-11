b63181bec0363ea9cbdbfba47edb977fba6b2a93
Aleksandr-74 <104676875+Aleksandr-74@users.noreply.github.com>
2022-09-11 20:29:27 +0500

    Delete pythonProjectNew directory

--- pythonProjectNew/ООП/Задание 3/User.py
+++
@@ -1,9 +0,0 @@
-class User():
-    def __init__(self):
-        self._email, self._password = input('Введите email и пароль\n').split()
-
-    def out(self):
-        return {"email": self._email, "password": self._password}
-
-    def info(self):
-        print(f"email: {self._email}, password: {self._password}")
