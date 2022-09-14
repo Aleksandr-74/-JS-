dd7b4abdc337faeaf9f3be3560eb08ff3a41ab10
Aleksandr <74simakovan@gmail.com>
2022-09-14 22:33:27 +0500

    sudoku c доработками

--- Sudoku/sudoky_app/sudoku/views.py
+++ Sudoku/sudoky_app/sudoku/views.py
@@ -1,25 +1,38 @@
+import copy
+
 from flask import render_template, request
 
 from sudoky_app import app
-from sudoky_app.sudoku.models import sudoku
+from sudoky_app.sudoku.models import sudoku, cops
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
+    cops = copy.copy(sudoku)
     if request.method == 'POST':
+
+        if request.form.get('clear'):
+            print('yu')
+            # cops = copy.deepcopy(sudoku)
+            for i in range(9):
+                for j in range(9):
+                    cops[i][j] = copy.deepcopy(sudoku[i][j])
+            print(cops)
+            print(sudoku)
+
+
         for i in range(9):
             for j in range(9):
-                el = request.form.get(str(i)+str(j))
-                print(el)
-                if el == '':
-                    sudoku[i][j] = 0
-                else:
-                    sudoku[i][j] = int(el)
+                if cops[i][j] == 0:
+                    el = request.form.get(str(i) + str(j))
+                    try:
+                        cops[i][j] = int(el)
+                    except:
+                        cops[i][j] = 0
 
 
 
 
-        print(sudoku)
 
 
     return render_template('index.html', sudoku=sudoku)
